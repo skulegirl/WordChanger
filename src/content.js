64af25327c0e3bcf470ec1e9cdc44cbfaee07f29
turtlemaster890 <78627075+turtlemaster890@users.noreply.github.com>
2021-03-07 18:38:42 -0500

    Added code to allow multiple search terms - incomplete (changing functionality still to come)

--- src/content.js
+++ src/content.js
@@ -1,6 +1,6 @@
 chrome.storage.sync.get({
     search1: '',
-    replace1: ''
+    replace1: '',
   }, function(items) {
     if (items.search1 !== '' && items.replace1 !== '') {
         let regex_val = new RegExp('\\b' + items.search1 + '\\b', 'gi');
--- src/options.js
+++ src/options.js
@@ -1,10 +1,43 @@
+var searchTerms = document.getElementById('searchTerms');
+var data = [];
+var i = 0;
+
+document.getElementById('add').addEventListener('click', function() {
+	var searchDiv = document.createElement('div');
+  var input = document.createElement('input');
+  var replace = document.createElement('input');
+  var minus = document.createElement('button')
+  input.setAttribute('type', 'text');
+  input.setAttribute('placeholder', 'Search Term');
+  replace.setAttribute('type', 'text');
+  replace.setAttribute('placeholder', 'Replace Term');
+  minus.textContent = '-';
+  searchDiv.classList.add('search-term')
+  searchDiv.setAttribute('id', 'search-Div-' + i);
+  minus.setAttribute('id', 'minus-Button-' + i)
+  minus.classList.add('minusButton')
+  minus.addEventListener('click', DeleteLine)
+  var button = document.createElement('button');
+  i++;
+  searchTerms.appendChild(searchDiv);
+  searchDiv.appendChild(input);
+  searchDiv.appendChild(replace);
+  searchDiv.appendChild(minus);
+});
+
+function DeleteLine(event) {
+	var targetButton = event.target;
+  var parentElement = targetButton.parentElement;
+  parentElement.remove();
+}
 // Saves options to chrome.storage
 function save_options() {
   var search1 = document.getElementById('search1').value;
   var replace1 = document.getElementById('replace1').value;
 chrome.storage.sync.set({
     search1: search1,
-    replace1: replace1
+    replace1: replace1,
+
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -21,7 +54,7 @@ function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
     search1: '',
-    replace1: ''
+    replace1: '',
   }, function(items) {
     document.getElementById('search1').value = items.search1;
     document.getElementById('replace1').value = items.replace1;
