6ba11c435dc7e8fcfbc6f3a6a7cef38feceee200
turtlemaster890 <78627075+turtlemaster890@users.noreply.github.com>
2021-03-07 19:27:41 -0500

    added multi-line saving/restoring

--- src/options.js
+++ src/options.js
@@ -1,9 +1,18 @@
 var searchTerms = document.getElementById('searchTerms');
 var data = [];
 var i = 0;
+console.log("hi")
 
-document.getElementById('add').addEventListener('click', function() {
-	var searchDiv = document.createElement('div');
+document.getElementById('add').addEventListener('click', AddLine);
+
+function DeleteLine(event) {
+	var targetButton = event.target;
+  var parentElement = targetButton.parentElement;
+  parentElement.remove();
+}
+function AddLine()
+{
+  var searchDiv = document.createElement('div');
   var input = document.createElement('input');
   var replace = document.createElement('input');
   var minus = document.createElement('button')
@@ -11,6 +20,8 @@ document.getElementById('add').addEventListener('click', function() {
   input.setAttribute('placeholder', 'Search Term');
   replace.setAttribute('type', 'text');
   replace.setAttribute('placeholder', 'Replace Term');
+  input.classList.add("searchInput");
+  replace.classList.add("replaceInput");
   minus.textContent = '-';
   searchDiv.classList.add('search-term')
   searchDiv.setAttribute('id', 'search-Div-' + i);
@@ -23,20 +34,17 @@ document.getElementById('add').addEventListener('click', function() {
   searchDiv.appendChild(input);
   searchDiv.appendChild(replace);
   searchDiv.appendChild(minus);
-});
-
-function DeleteLine(event) {
-	var targetButton = event.target;
-  var parentElement = targetButton.parentElement;
-  parentElement.remove();
+  return searchDiv;
 }
+
 // Saves options to chrome.storage
 function save_options() {
-  var search1 = document.getElementById('search1').value;
-  var replace1 = document.getElementById('replace1').value;
+  console.log("In save options")
+  var searches = Array.from(document.getElementsByClassName("searchInput")).map(x => x.value);
+  var replaces = Array.from(document.getElementsByClassName("replaceInput")).map(x => x.value);
 chrome.storage.sync.set({
-    search1: search1,
-    replace1: replace1,
+    searches: searches,
+    replaces: replaces,
 
   }, function() {
     // Update status to let user know options were saved.
@@ -53,11 +61,20 @@ chrome.storage.sync.set({
 function restore_options() {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    search1: '',
-    replace1: '',
+    searches: [''],
+    replaces: [''],
   }, function(items) {
-    document.getElementById('search1').value = items.search1;
-    document.getElementById('replace1').value = items.replace1;
+    items.searches.forEach( (element, i) => {
+      var searchDiv
+      if (i == 0){
+        searchDiv = document.getElementsByClassName("search-term")[0];
+      }
+      else {
+        searchDiv = AddLine();
+      }
+      Array.from(searchDiv.getElementsByClassName("searchInput")).forEach(x => x.value = element);
+      Array.from(searchDiv.getElementsByClassName("replaceInput")).forEach(x => x.value = items.replaces[i]);
+    });
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
