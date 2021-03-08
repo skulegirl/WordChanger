acc330f45b2fb720fb4c05aad443299f9ac3ae6e
turtlemaster890 <78627075+turtlemaster890@users.noreply.github.com>
2021-03-07 19:41:04 -0500

    Added changing the text in webpage!

--- src/content.js
+++ src/content.js
@@ -1,9 +1,13 @@
 chrome.storage.sync.get({
-    search1: '',
-    replace1: '',
+    searches: [''],
+    replaces: [''],
   }, function(items) {
-    if (items.search1 !== '' && items.replace1 !== '') {
-        let regex_val = new RegExp('\\b' + items.search1 + '\\b', 'gi');
-        document.body.innerHTML = document.body.innerHTML.replace(regex_val, items.replace1);
+    console.log("Test!!!!");
+    items.searches.forEach((element, i) => {
+      if (element !== '' && items.replaces[i] !== '') {
+        let regex_val = new RegExp('\\b' + element + '\\b', 'gi');
+        document.body.innerHTML = document.body.innerHTML.replace(regex_val, items.replaces[i]);
     }
+    });
+      
 });
